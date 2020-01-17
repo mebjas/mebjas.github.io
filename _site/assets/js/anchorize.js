2698e26e3243ac90c6342a65cf33cab00c3234e6
Minhaz A V <minhazav@google.com>
2020-01-17 22:41:10 +0800

    revert .nojekyll approach

--- _site/assets/js/anchorize.js
+++
@@ -1,4 +0,0 @@
-// Generate link anchors for all headlines.
-$('h2,h3,h4,h5,h6').filter('[id]').each(function () {
-    $(this).html('<a href="#'+$(this).attr('id')+'">' + $(this).text() + '</a>');
-});
